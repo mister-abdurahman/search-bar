e6400d2243f9bd46d0f90b2f91be2844c658e74c
aramide ramadan <aramramadan7@gmail.com>
2025-03-12 11:28:47 +0100

    working on search location part

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-50">
+      <div className="pt-8 flex justify-center">
+        <Search />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,138 @@
+import SearchIcon from "@mui/icons-material/Search";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useState } from "react";
+import { Autocomplete, TextField } from "@mui/material";
+
+const locations = [
+  "kondapur",
+  "Madhapur",
+  "Gachibowli",
+  "Hitech City",
+  "Kukatpally",
+  "Begumpet",
+  "Secunderabad",
+  "Banjara Hills",
+  "Jubilee Hills",
+  "Ameerpet",
+  "Somajiguda",
+  "Begumpet",
+  "Kothaguda",
+];
+
+function Search() {
+  const [openedLongSearchBar, setOpenedLongSearchBar] = useState(false);
+
+  function handleSearchBar() {
+    setOpenedLongSearchBar(!openedLongSearchBar);
+  }
+
+  return (
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        handleSearchBar();
+      }}
+      className={`bg-white rounded-2xl flex items-center pl-4 max-w-fit text-sm transform-all duration-500 ${
+        openedLongSearchBar ? "shadow-2xl" : "shadow-md"
+      }`}
+    >
+      <div className="flex flex-col py-3 border-r border-gray-300 pr-4">
+        <p
+          className={`text-rose-800 transform-all duration-500 ${
+            openedLongSearchBar ? "block" : "hidden"
+          }`}
+        >
+          Campaign Duration
+        </p>
+        <div className="space-x-2">
+          <span>Feb 6</span>
+          <span className="text-gray-500">to</span>
+          <span>Feb 12</span>
+          <span className="text-gray-400">|</span>
+          <span>1 week</span>
+        </div>
+      </div>
+      <div className="border-r border-gray-300 px-3 py-3">
+        <p
+          className={`text-rose-800 transform-all duration-500 ${
+            openedLongSearchBar ? "block" : "hidden"
+          }`}
+        >
+          Preffered Locations
+        </p>
+        {openedLongSearchBar ? (
+          <div className="flex items-center space-x-2">
+            <span className="text-gray-500 whitespace-nowrap">
+              Hyderbad <KeyboardArrowDownIcon />
+            </span>
+            <div className="w-[12rem]" onClick={(e) => e.stopPropagation()}>
+              <Autocomplete
+                multiple
+                id="tags-standard"
+                options={locations}
+                getOptionLabel={(option) => option}
+                defaultValue={[]}
+                renderTags={(value, getTagProps) =>
+                  value.map((option, index) => (
+                    <span
+                      key={index}
+                      {...getTagProps({ index })}
+                      className="bg-gray-200 rounded-full px-2 py-1 text-sm mr-1 mb-1"
+                    >
+                      {option}
+                    </span>
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    label=""
+                    placeholder="Search your locations"
+                    size="small"
+                    InputProps={{
+                      ...params.InputProps,
+                      disableUnderline: true,
+                      sx: { fontSize: "0.85rem" },
+                    }}
+                  />
+                )}
+                sx={{
+                  "& .MuiAutocomplete-tag": {
+                    display: "inline-flex",
+                    overflowX: "auto",
+                    whiteSpace: "nowrap",
+                  },
+                }}
+              />
+            </div>
+          </div>
+        ) : (
+          <>
+            {" "}
+            <span>Kondapur</span>
+            <span>+2</span>
+          </>
+        )}
+      </div>
+      <div className="px-3 py-3">
+        <p
+          className={`text-rose-800 transform-all duration-500 ${
+            openedLongSearchBar ? "block" : "hidden"
+          }`}
+        >
+          Radius
+        </p>
+        <span>5 km</span>
+      </div>
+      <div className=" text-white bg-rose-800 p-5 rounded-r-2xl">
+        <SearchIcon />
+        <span className={`${openedLongSearchBar ? "inline-block" : "hidden"}`}>
+          Search
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Search;

99e24b454efacba9a4cd565d4f92fd0806659302
aramide ramadan <aramramadan7@gmail.com>
2025-03-13 10:11:08 +0100

    cleaning code

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -8,16 +8,13 @@ export const calculateDateDifference = (
   dateString1: string | Date,
   dateString2: string | Date
 ) => {
-  // Parse the date strings into Date objects
   const date1 = new Date(dateString1);
   const date2 = new Date(dateString2);
 
-  // Calculate the absolute difference in days, weeks, and months
   const daysDifference = Math.abs(differenceInDays(date2, date1));
   const weeksDifference = Math.abs(differenceInWeeks(date2, date1));
   const monthsDifference = Math.abs(differenceInMonths(date2, date1));
 
-  // Determine the appropriate unit (days, weeks, or months)
   if (daysDifference < 7) {
     return `${daysDifference} day${daysDifference !== 1 ? "s" : ""}`;
   } else if (weeksDifference < 4) {
